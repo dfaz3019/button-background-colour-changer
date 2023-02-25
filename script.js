a3f9a3ebc03eb69393d87ea58a80b69303c9eaf2
Andrew Miller <andrew.william.miller7@gmail.com>
2023-02-25 16:17:50 +1300

    A test to see if the random logic can result in 0

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const colours = ['black','red','gold','teal','orange','yellow','bronze','lime','
 
 const button = document.querySelector('button');
 
+// testing if your random logic can result in the value of 0;
+const randTest = () => {
+    let zeroFound = false;
+    for(let i = 0; i < 10000; i++){
+        if(Math.floor(Math.random() * 10) + 1 == 0){
+            zeroFound = true;
+        }
+    }
+    let message = zeroFound ? "Zero Found!" : "Zero Not Found!";
+    alert(message);
+}
+randTest();
+
 button.addEventListener('click', () => {
     var randomColour = Math.floor(Math.random() * 10) + 1;
     const grabBody = document.querySelector('body')
