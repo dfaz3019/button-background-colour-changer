51ab933bb87e832e1abb92dff1408a90817f261e
Andrew Miller <andrew.william.miller7@gmail.com>
2023-02-25 16:29:54 +1300

    Moar comments :O

--- script.js
+++ script.js
@@ -7,16 +7,21 @@ const randTest = () => {
     let zeroFound = false;
     for(let i = 0; i < 10000; i++){
         if(Math.floor(Math.random() * 10) + 1 == 0){
-            zeroFound = true;
+            zeroFound = true; // if it's still not true after 10,000 iterations it's probably safe to assume it will never be 0 with this logic :P
         }
     }
+    // ternary ftw
     let message = zeroFound ? "Zero Found!" : "Zero Not Found!";
-    alert(message);
+    alert(message); // console.log is a better alternative when testing values :P
 }
 randTest();
 
+// suggested alternative random logic below
+// Returns a random integer from 0 to 10:
+// Math.floor(Math.random() * 11); 
+
 button.addEventListener('click', () => {
-    var randomColour = Math.floor(Math.random() * 10) + 1;
+    var randomColour = Math.floor(Math.random() * 10) + 1; // let keyword might be better than var here :O
     const grabBody = document.querySelector('body')
     grabBody.style.backgroundColor = colours[randomColour]
 })
