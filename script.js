570072e749226c8b389fa0774c38df643bf2d067
Dana Fazeli <danafazeli@hotmail.co.uk>
2023-02-25 14:08:24 +1100

    Fixed Event Listener

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const colours = ['black','red','gold','teal','orange','yellow','bronze','lime','green'];
 
-
-(function () {
-    randomColour = Math.floor(Math.random() * 10) + 1;
-})();
-
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-    const grabBody = document.querySelector('body');
-    grabBody.style.backgroundColor = colours[randomColour];
+    var randomColour = Math.floor(Math.random() * 10) + 1;
+    const grabBody = document.querySelector('body')
+grabBody.style.backgroundColor = colours[randomColour]
 })
 
